b544296d0586999c37228c93ea75f0d1c60eb40b
kelvinrobert091 <kelvinrobert091@gmail.com>
2025-12-12 06:36:11 +0000

    feat: Fix black screen issue on Android

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/dashboard/dashboard_screen.dart';
 import 'theme/app_theme.dart';
 import 'services/auth_service.dart';
 import 'services/notification_service.dart';
@@ -50,21 +52,23 @@ class AuthGate extends StatefulWidget {
 }
 
 class _AuthGateState extends State<AuthGate> {
-  StreamSubscription? _sub;
   @override
-  void initState() {
-    super.initState();
-    _sub = FirebaseAuth.instance.authStateChanges().listen((user) async {
-      if (user == null) {
-        AppRouter.goToLogin(context);
-      } else {
-        await NotificationService.registerShopDeviceToken(user.uid);
-        AppRouter.goToDashboard(context);
-      }
-    });
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        if (snapshot.hasData) {
+          return const DashboardScreen();
+        }
+        return const LoginScreen();
+      },
+    );
   }
-  @override
-  void dispose() { _sub?.cancel(); super.dispose(); }
-  @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: CircularProgressIndicator()));
 }
